8370f2d3f85451e87f7480b8cb073cf07b4ee9d3
sávio <saviopf22@gmail.com>
2019-03-17 23:39:29 -0300

    crud teste terminado

--- conexao.php
+++ conexao.php
@@ -1,9 +1,23 @@
 <?php
 
-# constantes para guardar parametros da conexão com o BD
-define('HOST', 'localhost');
-define('USER', 'root');
-define('SENHA', '1234');
-define('DB', 'estoque');
+/** documentação
+ * 
+ * @return \PDO
+ */
 
-$conexao = 
+# constantes para guardar parametros da conexão com o BD
+define('DSN', 'mysql:host=localhost;dbname=estoque;charset=utf8');
+define('USER', 'root');
+define('SENHA', '');
+
+function getConexao(){
+    try{
+        $conexao = new PDO(DSN, USER, SENHA, NULL);
+        
+        return $conexao;
+        //echo "teste de conexão";
+    }catch(PDOException $error){
+        echo "Erro: ".$error->getMessage();
+    }
+}
+
