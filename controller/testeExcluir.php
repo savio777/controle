2306b3ec305063551d48970ab7161da4969d9412
sávio <saviopf22@gmail.com>
2019-04-03 15:47:21 -0300

    funcao apagar funcionando, falta refatorar, editar registros e pagina de cadastro de produtos

--- controller/testeExcluir.php
+++ controller/testeExcluir.php
@@ -1,11 +1,12 @@
 <?php
 
-require_once '../crud';
+require_once '../crud.php';
 
 $crud = new Crud();
 
 if($_GET){
     $crud->apagarProduto($_GET['id']);
+    header('Location: ../pages/estoque.php');
 }else{
     echo('nenhuma requisição');
 }
