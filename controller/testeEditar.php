1a2f62787fcb9591492713bdc622e7de347ad4f6
savio <saviopf22@gmail.com>
2019-05-27 03:06:51 -0300

    arquivo excluido errado

--- controller/testeEditar.php
+++ controller/testeEditar.php
@@ -1,20 +1,17 @@
 <?php
 
-require_once('../crud/crudProduto.php'); 
+require_once('../crud/crudProduto.php');
 require_once('../models/produto.php');
 
 $produto = NULL;
 $crud = NULL;
 
-if($_POST){
+if ($_POST) {
     $produto = new Produto($_POST['nomeProduto'], $_POST['preco'], $_POST['quantidade']);
     $produto->setId($_POST['id']);
-
     $crud = new CrudProduto();
     $crud->editarProduto($produto);
-
     header('Location: ../pages/estoque.php');
-}else{
-    echo('nenhuma requisição');
+} else {
+    echo ('nenhuma requisição');
 }
-
