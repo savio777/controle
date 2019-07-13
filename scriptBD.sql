253f6598095d6275ee298819201e759e59889966
Savio <saviopf22@gmail.com>
2019-07-13 20:29:12 -0300

    mudanças no atributo do usuario

--- scriptBD.sql
+++ scriptBD.sql
@@ -4,7 +4,9 @@ USE estoque;
 
 CREATE TABLE usuario(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nomeUser VARCHAR(100) NOT NULL UNIQUE,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    tipo_user VALUES(30) NOT NULL,  -- root, adm, comum
     senha VARCHAR(100) NOT NULL,
     permissaoRoot VARCHAR(1) -- 's' ou 'n'
 );
